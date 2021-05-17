95fa94225863195c4dd1652cb5e8d2757bdaae1b
Fachri Hawari <fachri.hawari@gmail.com>
2021-05-17 23:00:59 +0700

    feat: Setup Test (#5)
    * feat: Setup Unit Test
    
    * chore: Add test command

---
+++ setupTests.js
@@ -0,0 +1,19 @@
+jest.useFakeTimers();
+
+jest.mock('react-native/Libraries/ReactNative/AppRegistry');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
+jest.mock('react-native/Libraries/Linking/Linking', () => {
+  // we need to mock both Linking.getInitialURL()
+  // and Linking.getInitialURL().then()
+  const getInitialURL = jest.fn();
+  getInitialURL.mockReturnValueOnce({ then: jest.fn() });
+
+  return {
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    openURL: jest.fn(),
+    canOpenURL: jest.fn(),
+    getInitialURL: getInitialURL,
+  };
+});
